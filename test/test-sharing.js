ba4593e1ddadb119c70dc4e6e9a953d21056b61e
Antonio Lain <antlai@cafjs.com>
2015-06-23 22:55:56 -0700

    Fix test to accomodate for replica update latency

--- test/test-sharing.js
+++ test/test-sharing.js
@@ -52,7 +52,7 @@ module.exports = {
             this.$.top.__ca_graceful_shutdown__(null, cb);
         }
     },
-/*
+
     sharedMap: function (test) {
        test.expect(23);
 
@@ -394,7 +394,6 @@ module.exports = {
         });
 
     },
-*/
     oneAggregate: function(test) {
         var self = this;
         test.expect(12);
